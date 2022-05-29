cd7ff4127a10d0741d93921517139c3984b6572c
Roelant Dieben <roelant@icyt.nl>
2022-05-29 11:52:52 +0200

    Add core 3.1 project

---
+++ Migration.Demo.Core31/Migration.Demo.Data/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace Migration.Demo.Data
+{
+    public static class SqlHelper
+    {
+        public static string _connectionString = "Server=(localdb)\\MSSQLLocalDB;Database=MigrationDemo;Integrated Security=true;";
+
+        public static int GetValueFromDatabase()
+        {
+            using (var conn = new SqlConnection(_connectionString))
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "SELECT TOP 1 [Value] FROM [dbo].[Demo]";
+                    var value = int.Parse(cmd.ExecuteScalar().ToString());
+
+                    return value;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
